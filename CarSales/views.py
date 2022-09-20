c078b4a818eabeffe166073a25c14dbfc1570f69
Łukasz Radomiński <lukaszr82@o2.pl>
2022-09-20 15:52:32 +0200

    Load First Html Page

--- CarSales/views.py
+++ CarSales/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def carslist(request):
+    return render(request, 'carslist.html')  # we define the htmls to render for each route.
