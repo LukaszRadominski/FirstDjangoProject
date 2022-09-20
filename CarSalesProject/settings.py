bc0c917e62c25296f5783111ad221aee2a4295c7
Łukasz Radomiński <lukaszr82@o2.pl>
2022-09-20 15:10:44 +0200

    Add CarSales app to settings

--- CarSalesProject/settings.py
+++ CarSalesProject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'CarSales'  # dodane w kroku 5.3 READEME.txt
 ]
 
 MIDDLEWARE = [
