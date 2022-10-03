47bab2b375655cf8ecd5ed3be2724a51c9d9db32
Łukasz Radomiński <lukaszr82@o2.pl>
2022-10-03 13:46:25 +0200

    Add description first Oracle DB Setup and Create Connection

--- CarSales/views.py
+++ CarSales/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def carslist(request):
-    return render(request, 'carslist.html')  # we define the htmls to render for each route.
+    return render(request, 'carslist.html')  # 6.6. we define the htmls to render for each route.
